326c14b2c363d13426eae1d7256a66b07ddc25c3
master cassidy <66987878+pydox@users.noreply.github.com>
2020-11-16 15:03:27 -0500

    Update ec2instance.tf

--- ec2instance.tf
+++ ec2instance.tf
@@ -1,10 +1,9 @@
 provider "aws" {
   region     = "us-west-2"
-  access_key = "AKIA5V3RNY2DIFANQT4L"
-  secret_key = ""
+  access_key = "AKIA5V3RNY2DOOSVVEWQ"
+  secret_key = "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"
 }
-
-resource "aws_instance" "myec2" {
-   ami = "ami-082b5a644766e0e6f"
-   instance_type = "t2.micro"
-}
+resource "aws_instance" "my-instance" {
+  count         = "${var.instance_count}"
+  ami           = "${lookup(var.ami,var.aws_region)}"
+  instance_type = "${var.instance_type}
